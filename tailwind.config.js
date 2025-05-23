821ba90b313ed7b167806c048117d2572ec5b19e
Itzgregory <gregorychuks0@gmail.com>
2025-05-23 22:23:14 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E90FF',
+      },
+      fontFamily: {
+        spaceMono: ['SpaceMono', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
